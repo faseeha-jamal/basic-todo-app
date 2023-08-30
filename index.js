9185616b48301491bf6a8fe6a8a51ca3afb567d1
faseeha-jamal <jaseehajamal08@gmail.com>
2023-08-30 16:45:12 +0530

    feat:implement edit option

--- index.js
+++ index.js
@@ -1,89 +1,78 @@
-let showItems=document.querySelector(".out-container");
-let err=document.querySelector(".err")
-let inpVal=document.querySelector(".inp");
-let addBtn=document.querySelector(".btn");
-let i=1;
-if (!localStorage.getItem("data")) {
-  localStorage.setItem("data", JSON.stringify([]));
-}
+let showItems = document.querySelector(".out-container");
+let err = document.querySelector(".err");
+let inpVal = document.querySelector(".inp");
+let addBtn = document.querySelector(".btn");
+let i = 1;
+// import localStorage
+todos = JSON.parse(localStorage.getItem("data")) || [];
 
 createItem();
-
-inpVal.addEventListener("keydown",(e)=>{
-    // console.log(e.key);
-  if(e.key=="Enter"){
-     inpErr()
+// enter key event
+inpVal.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    inpErr();
   }
-})
+});
+// add buttone event
+addBtn.addEventListener("click", () => {
+  inpErr();
+});
 
-addBtn.addEventListener("click",()=>{ 
-   inpErr()
-})
+function inpErr() {
+  if (inpVal.value) {
+    let arr = { item: inpVal.value, id: i++ };
+    todos.push(arr);
 
-function inpErr(){
-if(inpVal.value){
-  let arr={item:inpVal.value,id:i++}
-  //  showItems.innerHTML=""
- 
-   let newArr=JSON.parse(localStorage.getItem("data"))
-   newArr.push(arr)
-
-   localStorage.setItem("data",JSON.stringify(newArr));
-    console.log(newArr)
-      inpVal.value=""
-      createItem()
-      err.innerText=""
-  }else{
-    err.innerText="Enter You'r item"
+    localStorage.setItem("data", JSON.stringify(todos));
+    inpVal.value = "";
+    createItem();
+    err.innerText = "";
+  } else {
+    err.innerText = "Enter You'r item";
   }
 }
+// Creat elements
+function createItem() {
+  showItems.innerHTML = "";
+  inpVal.value = "";
 
-function createItem(){
-  let arrItm=JSON.parse(localStorage.getItem("data")||{item:""})
-  console.log(arrItm);
-  showItems.innerHTML=""
-        inpVal.value=""
-    
-    arrItm.forEach((obj)=>{
-        // console.log("hello");
-        let div=document.createElement("div")
-        let list=document.createElement("h3")
-        let upd=document.createElement("button")
-        let del=document.createElement("button")
-         
-        div.className="list"
-        list.className="item"
-        list.innerText=obj.item
-        upd.innerText="Update"
-        del.innerText="Remov"
-        // console.log(list);
-        del.addEventListener("click",()=>{
-          let filData=arrItm.filter((itm)=>{
-            return itm.id!=obj.id
-          })
-          localStorage.setItem("data",JSON.stringify(filData))
-          createItem()
-        })
+  todos.forEach((obj) => {
+    console.log(obj);
+    let div = document.createElement("div");
+    let list = document.createElement("div");
+    let upd = document.createElement("button");
+    let del = document.createElement("button");
 
-        upd.addEventListener("click",()=>{
-            list.innerHTML=`<input type="text" value="${obj.item}">`;
-          list.addEventListener("change",(e)=>{
-            let val={item:e.target.value}
-            console.log(val);
-            localStorage.setItem("data",JSON.stringify(val))
+    div.className = "list";
+    list.className = "item";
 
-          }) 
-            createItem()
-        })
-    
-        div.append(list,upd,del)
-        showItems.appendChild(div)
-    
+    list.innerHTML = `<input type="text" value='${obj.item}' readonly />`;
 
-    })
+    upd.innerText = "Update";
+    del.innerText = "Remov";
+
+    console.log(list);
+
+    div.append(list, upd, del);
+    showItems.appendChild(div);
+    // Remove section
+    del.addEventListener("click", () => {
+      todos = todos.filter((itm) => itm != obj);
+      localStorage.setItem("data", JSON.stringify(todos));
+      createItem();
+    });
+    // Edit section
+    upd.addEventListener("click", (e) => {
+      const input = list.children[0];
+      input.removeAttribute("readonly");
+      input.focus();
+
+      input.addEventListener("blur", (e) => {
+        console.log("hello");
+        obj.item = e.target.value;
+        localStorage.setItem("data", JSON.stringify(todos));
+        createItem();
+      });
+    });
+  });
 }
-
-
-
-
-
